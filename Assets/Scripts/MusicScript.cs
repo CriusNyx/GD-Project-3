56e10a1a215a38a174b6014f63ed65aece1e2a21
Murilo Delgado <mgdelgado@oakland.edu>
2020-10-26 20:44:05 -0400

    Main Menu Music

---
+++ Assets/Scripts/MusicScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour
+{
+    private AudioSource audio;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (audio.isPlaying) return;
+        audio.Play();
+    }
+
+    public void StopMusic()
+    {
+        audio.Stop();
+    }
+
+}
